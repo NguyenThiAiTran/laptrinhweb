1424c1701b7542b25a5fa3b15cf754ba2a3d90a8
Nguyen Thi Ai Tran <ntatran2101259@student.ctuet.edu.vn>
2024-09-19 16:09:40 +0700

    Initial commit

--- server.js
+++ server.js
@@ -2,21 +2,29 @@ import express from 'express'
 import dotenv from 'dotenv/config'
 import myDateTime from './date'
 import getURL from './getURL'
+import viewEngine from './viewEngine'
+
 const app = express()
+viewEngine(app)
 const port=process.env.PORT
+
 app.get('/', (req, res) => {
-    res.send('Hello, tôi tên là Nguyễn Thị Ái Trân')
+    res.send('Hello, My name is Ái Trân')
 })
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
 app.get('/about', (req, res) => {
-    res.send('Xin chào, tôi là Ái Trân')
+    res.send('Xin chào, tên của tôi là Nguyễn Thị Ái Trân')
 })
+
 app.get('/date', (req, res) => {
     const date = myDateTime();
     res.send(date);
 });
+
 app.get('/geturl', (req, res) => {
     const getParamsURL = getURL.getParamsURL(req);
     res.send(getParamsURL);
@@ -25,4 +33,16 @@ app.get('/geturl', (req, res) => {
 app.get('/geturl', (req, res) => {
     const getPath = getURL.getPath(req);
     res.send(getPath);
+})
+
+app.get('/ejs', (req, res) => { 
+    res.render("test")
+})
+
+app.get('/homeejs', (req, res) => { 
+    res.render("home")
+})
+
+app.get('/aboutejs', (req, res) => { 
+    res.render("about")
 })
---
+++ viewEngine.js
@@ -0,0 +1,5 @@
+const viewEngine=(app) => {
+    app.set("view engine", "ejs")
+    app.set("views","./")
+    }
+export default viewEngine
